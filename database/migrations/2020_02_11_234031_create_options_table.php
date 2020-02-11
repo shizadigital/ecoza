8099496972586ddf2060c254742aa1efa9ad752f
Afrioni Jaya Saputra <afrioni@afrioni.web.id>
2020-02-12 00:23:22 +0700

    add: new option data

---
+++ database/migrations/2020_02_11_234031_create_options_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('options', function (Blueprint $table) {
+            $table->bigIncrements('optId');
+            $table->string('optName');
+            $table->longText('optValue')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('options');
+    }
+}

a455ae0da98748d6d37a365dc39a75a4e5b4b1e1
Afrioni Jaya Saputra <afrioni@afrioni.web.id>
2020-02-17 00:26:12 +0700

    update: change new framework

--- application/libraries/AdminEnv.php
+++ application/libraries/AdminEnv.php
@@ -238,12 +238,12 @@ class AdminEnv {
                         }
 
                         if($permission == true){
-                            echo '<a href="'.$btn_v['access'].'" class="btn btn-primary btn-with-addon mr-auto text-nowrap d-none d-md-block"><span class="btn-addon"><i class="';
+                            echo '<a href="'.$btn_v['access'].'" class="btn btn-primary btn-with-addon mr-auto text-nowrap d-none d-md-block"><span class="btn-addon"><i class="btn-addon-icon ';
                             if(empty($btn_v['icon'])){ echo 'fe fe-menu'; } else { echo $btn_v['icon']; } 
                             echo '"></i></span> '.$btn_v['title'].'</a>';
                         }
                     } else {
-                        echo '<a href="'.$btn_v['access'].'" class="btn btn-link btn-float text-default"><span class="btn-addon"><i class="';
+                        echo '<a href="'.$btn_v['access'].'" class="btn btn-primary btn-with-addon mr-auto text-nowrap d-none d-md-block"><span class="btn-addon"><i class="btn-addon-icon ';
                         if(empty($btn_v['icon'])){ echo 'fe fe-menu'; } else { echo $btn_v['icon']; } 
                         echo '"></i></span>'.$btn_v['title'].'</a>';
                     }

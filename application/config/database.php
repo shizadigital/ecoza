588e53065c91065a6986cc7630395ee425aeee65
Abinowo <abinowo.ananta@gmail.com>
2020-02-22 18:25:50 +0700

    fix session path

--- application/config/database.php
+++ application/config/database.php
@@ -74,10 +74,10 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 $db['default'] = array(
-	'dsn'	=> 'mysql:host=localhost; dbname=shiza_ecoza; charset=utf8;',
+	'dsn'	=> 'mysql:host=localhost; dbname=_shiza_ecoza; charset=utf8;',
 	'hostname' => 'localhost',
 	'username' => 'root',
-	'password' => '',
+	'password' => 'pass',
 	'database' => '',
 	'dbdriver' => 'pdo',
 	'dbprefix' => 'shiza_',

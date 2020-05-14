c665fa9be78c131edf5326dff62ca50fed9d9bd2
afrioni21 <afrioni@afrioni.web.id>
2020-05-15 06:39:54 +0700

    courier onprogress

--- application/modules/courier/controllers/admin/Courier.php
+++ application/modules/courier/controllers/admin/Courier.php
@@ -4,6 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Courier extends CI_Controller{ 
 
 	private $moduleName = '';
+	private $extensi_allowed = '';
 
 	public function __construct(){
 		parent::__construct();
@@ -16,6 +17,9 @@ class Courier extends CI_Controller{
 
 		// define module name variable
 		$this->moduleName = t( array('table'=>'users_menu', 'field'=>'menuName', 'id'=> 26) );
+
+		// file extention allowed
+		$this->extensi_allowed = array('jpg','jpeg','png','gif');
 
 		// load model
 		$this->load->model('courier_model');
@@ -64,9 +68,134 @@ class Courier extends CI_Controller{
 													'permission' => 'view'
 												)
 											),
+							'imgextention' => $this->extensi_allowed,
 						);
 
 			$this->load->view( admin_root('courier_add'), $data );
+		}
+	}
+
+	public function addprocess(){
+		if( is_add() ){
+			$error = false;
+
+			$country 	= empty($this->input->post('country'))?array():$this->input->post('country');
+			$zone 		= empty($this->input->post('zone'))?array():$this->input->post('zone');
+
+			if( empty( $this->input->post('couriername') ) OR count($country)<1 OR count($zone)<1 ){
+				$error = t('emptyrequiredfield');
+			}
+
+			if(count($country)>0){
+				foreach(array_filter($country) as $ky => $vl){
+					if( empty( $zone[$ky] ) ){
+						$error = t('emptyrequiredfield'); break;
+					}
+				}
+			}
+
+			if( !empty( $this->input->post('urltracking') ) ){
+				if(!filter_var($this->input->post('urltracking'), FILTER_VALIDATE_URL)){
+					$error = t('urlnotvalid');
+				}
+			}
+
+			// check image upload
+			if(!empty($_FILES['logo']['tmp_name'])){
+				$ext_file = strtolower(pathinfo($_FILES['logo']['name'], PATHINFO_EXTENSION));
+
+				if(!in_array($ext_file,$this->extensi_allowed)) {
+					$error = "<strong>".t('error')."!!</strong> " . t('wrongextentionfile');
+				}
+			}
+
+			if(getLengthDefault('id') < 1){
+				$error = "<strong>".t('error')."!!</strong> " . t('defaultlengthempty');
+			}
+
+			if(getWeightDefault('id') < 1){
+				$error = "<strong>".t('error')."!!</strong> " . t('defaultweightempty');
+			}
+
+			if(!$error){
+				$couriername 	= esc_sql( filter_txt( $this->input->post('couriername') ) );
+				$couriercode 	= esc_sql( filter_txt( $this->input->post('couriercode') ) );
+				$urltracking 	= filter_txt( $this->input->post('urltracking', true) );
+				$freeshipping	= ($this->input->post('freeshipping') =='1' ) ? 'y':'n';
+				$active	= ($this->input->post('active') =='1' ) ? 1:0;
+				$maxpackagewidth = singleComma(esc_sql(filter_txt( $this->input->post('maxpackagewidth') )));
+				$maxpackageheight = singleComma(esc_sql(filter_txt( $this->input->post('maxpackageheight') )));
+				$maxpackagelength = singleComma(esc_sql(filter_txt( $this->input->post('maxpackagelength') )));
+				$maxpackageweight = singleComma(esc_sql(filter_txt( $this->input->post('maxpackageweight') )));
+
+				// getId
+				$nextId = getNextId('courierId', 'courier');
+
+				// now
+				$now = time2timestamp();
+
+				$file_img = '';
+				$file_dir = '';
+				// upload image proccess
+				if(!empty($_FILES['logo']['tmp_name'])){
+					$ext_file = strtolower(pathinfo($_FILES['logo']['name'], PATHINFO_EXTENSION));
+
+					if(in_array($ext_file,$this->extensi_allowed)) {
+						$sizeimg = array(
+							'xsmall' 	=>'90',
+							'small' 	=>'210',
+							'medium' 	=>'530',
+							'large' 	=>'890'
+						);
+						$img = uploadImage('logo', 'courierlogo', $sizeimg, $this->extensi_allowed);
+						$file_img = $img['filename'];
+						$file_dir = $img['directory'];
+					}
+				}
+
+				$data = array(
+					'courierId' => $nextId,
+					'addonsId' => 0,
+					'courierName' => $couriername,
+					'courierCode' => $couriercode,
+					'courierUrlTracking' => $urltracking,
+					'lengthId' => getLengthDefault('id'),
+					'courierMaxLength' => $maxpackagelength,
+					'courierMaxWidth' => $maxpackagewidth,
+					'courierMaxHeight' => $maxpackageheight,
+					'courierMaxWeight' => $maxpackageweight,
+					'weightId' => getWeightDefault('id'),
+					'courierDirLogo' =>$file_dir,
+					'courierFileLogo' =>$file_img,
+					'courierFreeShipping' => $freeshipping,
+					'courierStatus' => $active,
+					'courierAddedDate' => $now,
+					'courierDeleted' => 0
+				);
+				
+				// insert data
+				$query = $this->Env_model->insert('stock_entry', $data);
+				
+			    if($query){
+
+					// add cost
+
+
+
+
+					$this->session->set_flashdata( 'succeed', t('successfullyadd'));
+					echo '<script>window.location.href = "'.admin_url('stock_entry').'";</script>';
+				} else {
+					// error condition
+					// format: type|title|description
+					echo 'danger|'.t('error').'|'.t('cannotprocessdata');
+				}
+				
+			} else {
+				// error condition
+				// format: type|title|description
+				echo 'danger|'.t('error').'|'.$error;
+			}
 		}
 	}
 
@@ -102,6 +231,105 @@ class Courier extends CI_Controller{
 	public function delete($id){
 		if( is_delete() ){
 
+		}
+	}
+
+	public function ajax_addnewservice(){
+		if( is_add() AND $this->input->post('CP') === get_cookie('sz_token')){
+			$error = false;
+
+			$idrow = generate_code(8);
+
+			$wherecountry = "countryDeleted='0' AND countryStatus='1'";
+			$country = $this->Env_model->view_where_order('countryId,countryName','geo_country',$wherecountry,'countryName','ASC');
+
+			$wherezone = "zoneDeleted='0' AND zoneStatus='1' AND countryId='".getCountryDefault('id')."'";
+			$zone = $this->Env_model->view_where_order('zoneId,zoneName','geo_zone',$wherezone,'zoneName','ASC');
+
+			echo '
+			<tr id="rowval-'.$idrow.'">
+				<td class="align-center">
+					<select name="country['.$idrow.']" class="custom-select" id="select2product'.$idrow.'">';
+					foreach($country AS $v){
+						echo '<option value="'.$v['countryId'].'"';
+						if( getCountryDefault('id') == $v['countryId'] ){ echo ' selected="selected"'; }
+						echo '>'.$v['countryName'].'</option>';
+					}
+					echo '
+					</select>
+				</td>
+				<td class="align-center" id="zoneinput-'.$idrow.'">
+					<select name="zone['.$idrow.']" class="custom-select">';
+						echo '<option value="">-- '.t('choosecountryfirst').' --</option>';
+						foreach($zone AS $vz){
+							echo '<option value="'.$vz['zoneId'].'">'.$vz['zoneName'].'</option>';
+						}
+					echo '
+					</select>
+				</td>
+				<td class="text-center">
+					<input type="text" name="servicename['.$idrow.']" class="form-control">
+				</td>
+				<td class="align-center">
+					<input type="text" onkeypress="return isNumberComma(event)" name="cost['.$idrow.']" class="form-control">
+				</td>
+				<td class="align-center">
+					<input type="text" onkeypress="return isNumberKey(event)" name="etd['.$idrow.']" class="form-control">
+				</td>
+				<td class="align-center">
+					<input type="text" name="note['.$idrow.']" class="form-control">
+				</td>
+				<td class="align-center">
+					<button class="btn btn-link" id="deleterow-'.$idrow.'" type="button"><i class="text-danger fe fe-trash-2"></i></button>
+
+					<script type="text/javascript">
+					$( document ).ready(function() {
+						$(\'#deleterow-'.$idrow.'\').click(function() {
+							$( "#rowval-'.$idrow.'" ).remove();
+						});
+
+						$(\'#select2product'.$idrow.'\').change(function() {
+							$(\'#zoneinput-'.$idrow.'\').html(\'<div class="text-center"><img src="'.web_assets('img/loader/loading.gif').'" alt="loader"></div>\');
+
+							var idzone = $(this).val();
+
+							// proccess
+							$.post("'.admin_url($this->uri->segment(2) . '/ajax_getzone').'", 
+							{
+								id: idzone,
+								idrow: \''.$idrow.'\',
+								CP: \''.get_cookie('sz_token').'\'
+								'.( is_csrf() ? ','.$this->security->get_csrf_token_name().':"'.$this->security->get_csrf_hash().'"':'').'
+							},
+							function(data){								
+								
+							}).done( function(data){
+								$(\'#zoneinput-'.$idrow.'\').html(data);
+							});
+						});
+						
+					});
+					</script>
+				</td>
+			</tr>
+			';
+		}
+	}
+
+	public function ajax_getzone(){
+		if( $this->input->post('CP') === get_cookie('sz_token')){
+			$id = esc_sql( filter_int( $this->input->post('id', true) ) );
+			$idrow = esc_sql( filter_txt( $this->input->post('idrow', true) ) );
+
+			$wherezone = "zoneDeleted='0' AND zoneStatus='1' AND countryId='".$id."'";
+			$zone = $this->Env_model->view_where_order('zoneId,zoneName','geo_zone',$wherezone,'zoneName','ASC');
+
+			echo '<select name="zone['.$idrow.']" class="custom-select">';
+			echo '<option value="">-- '.t('choosecountryfirst').' --</option>';
+			foreach($zone AS $vz){
+				echo '<option value="'.$vz['zoneId'].'">'.$vz['zoneName'].'</option>';
+			}
+			echo '</select>';
 		}
 	}
 
--- application/modules/courier/views/admin/courier_add.php
+++ application/modules/courier/views/admin/courier_add.php
@@ -5,6 +5,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 Register style (CSS)
 ************************************/
 $request_css_files = array(
+    'vendors/select2/dist/css/select2.min.css',
 );
 $request_style = "";
 $this->assetsloc->reg_admin_style($request_css_files,$request_style);
@@ -13,14 +14,240 @@ $this->assetsloc->reg_admin_style($request_css_files,$request_style);
 Register Script (JavaScript)
 *******************************************/
 $request_script_files = array(
+    'vendors/select2/dist/js/select2.full.min.js',
 );
-$request_script = "";
+$request_script = "
+$( document ).ready(function() {
+    $('.select2').select2();
+
+    // submit data
+    ajaxSubmit('#valid');     
+});
+";
 $this->assetsloc->reg_admin_script($request_script_files,$request_script);
 
 include V_ADMIN_PATH . "header.php";
 include V_ADMIN_PATH . "sidebar.php";
 include V_ADMIN_PATH . "topbar.php";
 
-// Your code of view here
+if( is_add() ):
+echo form_open( admin_url( $this->uri->segment(2) . '/addprocess'), array( 'id'=>'valid' ) );
+?>
+<div class="row">
+	<?php 
+	if( !empty( $this->session->has_userdata('succeed') ) ){
+		echo '<div class="col-md-12">
+		<div class="alert alert-icon alert-success alert-dismissible fade show" role="alert">
+			<i class="fa fa-check"></i> ' . $this->session->flashdata('succeed') . '
+			<button type="button" class="close" data-dismiss="alert" aria-label="Close"><i class="fe fe-x"></i></button>
+		</div>
+		</div>
+		';
+	}
+	if( !empty( $this->session->has_userdata('failed') ) ){
+		echo '<div class="col-md-12">
+		<div class="alert alert-icon alert-danger alert-dismissible fade show" role="alert">
+			<i class="fa fa-times"></i> ' . $this->session->flashdata('failed') . '
+			<button type="button" class="close" data-dismiss="alert" aria-label="Close"><i class="fe fe-x"></i></button>
+		</div>
+		</div>
+		';
+	}
+    ?>
+
+    <div class="col-md-12">
+
+        <div class="card card-statistics">
+            <div class="card-header">
+                <div class="card-heading">
+                    <h5 class="card-title mb-0"><?php echo t('addnew'); ?></h5>
+                </div>
+            </div>
+
+            <div class="card-body">
+                <div class="row">
+
+                    <div class="col-md-12">
+                        <h5 class="mb-4"><?php echo t('generalsetting'); ?></h5>
+                        <?php
+                        $imgext = '';
+                        $countimg = count($imgextention);
+                        $ximg = 1;
+                        foreach( $imgextention as $extimg ){
+                            $imgext .= $extimg;
+                            if($ximg != $countimg){
+                                $imgext .= ', ';
+                            }
+                            $ximg++;
+                        }
+
+                        // make input structure
+                        $inputs =  array(
+                            array(
+                                'type' => 'text',
+                                'label' => t('couriername').' <span class="text-red">*</span>',
+                                'name' => 'couriername',
+                            ),
+                            array(
+                                'type' => 'text',
+                                'label' => t('couriercode'),
+                                'name' => 'couriercode',
+                            ),
+                            array(
+                                'type' => 'text',
+                                'label' => t('urltracking'),
+                                'name' => 'urltracking',
+                            ),
+                            array(
+                                'type' => 'file-img',
+                                'label' => t('logo'),
+                                'name' => 'logo',
+                                'help' => t('infomainimg').' '.$imgext,
+                            ),
+                            array(
+                                'type' => 'checkbox',
+                                'label' => t('freeshipping'),
+                                'name' => 'freeshipping',
+                                'value' => '1',
+                                'title' => t('yes'),
+                                'checked' => false
+                            ),
+                            array(
+                                'type' => 'checkbox',
+                                'label' => t('active'),
+                                'name' => 'active',
+                                'value' => '1',
+                                'title' => t('yes'),
+                                'checked' => true
+                            ),
+                        );
+                        $this->formcontrol->buildInputs($inputs, 'horizontal', array('label'=>'col-md-2', 'input'=>'col-md-9'));
+                        ?>
+                        <hr/>
+                    </div>
+
+                    <div class="col-md-12">
+                        <h5 class="mb-4"><?php echo t('sizeandweightsetting'); ?></h5>
+                        <?php
+                        // make input structure
+                        $inputscostmax =  array(
+                            array(
+                                'type' => 'text',
+                                'label' => t('maxpackagewidth'),
+                                'name' => 'maxpackagewidth',
+                                'onkeypress'=>'return isNumberComma(event)',
+                                'value' => 0,
+                                'input-group' => array(
+                                    'prepend'=> getLengthDefault(),
+                                )
+                            ),
+                            array(
+                                'type' => 'text',
+                                'label' => t('maxpackageheight'),
+                                'name' => 'maxpackageheight',
+                                'onkeypress'=>'return isNumberComma(event)',
+                                'value' => 0,
+                                'input-group' => array(
+                                    'prepend'=> getLengthDefault(),
+                                )
+                            ),
+                            array(
+                                'type' => 'text',
+                                'label' => t('maxpackagelength'),
+                                'name' => 'maxpackagelength',
+                                'onkeypress'=>'return isNumberComma(event)',
+                                'value' => 0,
+                                'input-group' => array(
+                                    'prepend'=> getLengthDefault(),
+                                )
+                            ),
+                            array(
+                                'type' => 'text',
+                                'label' => t('maxpackageweight'),
+                                'name' => 'maxpackageweight',
+                                'onkeypress'=>'return isNumberComma(event)',
+                                'value' => 0,
+                                'input-group' => array(
+                                    'prepend'=> getWeightDefault(),
+                                )
+                            ),
+                        );
+                        $this->formcontrol->buildInputs($inputscostmax, 'horizontal', array('label'=>'col-md-2', 'input'=>'col-md-9'));
+                        ?>
+                        <hr/>
+                    </div>
+                    
+                    <div class="col-md-12">
+                        <h5 class="mb-4"><?php echo t('shippinglocationandcost'); ?></h5>
+                        <div class="d-block mb-3 mt-4">
+							<button type="button" class="btn btn-rounded btn-light" id="addattrvalue"><i class="fe fe-plus mr-md-1"></i> <?php echo t('addservice'); ?></button>
+							<script type="text/javascript">
+							$( document ).ready(function() {
+								$('#addattrvalue').click(function() {
+									// Disable button
+									$(this).attr('disabled','disabled');
+									$(this).addClass('btn-disabled');
+
+                                    $('#loading_ajax').show();
+
+									// proccess
+									$.post("<?php echo admin_url($this->uri->segment(2) . '/ajax_addnewservice'); ?>", 
+                                    {
+                                        CP: '<?php echo get_cookie('sz_token'); ?>'
+                                        <?php echo ( is_csrf() ? ','.$this->security->get_csrf_token_name().':"'.$this->security->get_csrf_hash().'"':''); ?>
+                                    },
+									function(data){
+										if( $('tr#novalfound').length ){
+											$('tr#novalfound').remove();
+										}
+
+										$('#addattrvalue').removeAttr('disabled');
+										$('#addattrvalue').removeClass('btn-disabled');
+									}).done( function(data){
+                                        $('#loading_ajax').hide();
+
+										$('.servicetable tbody').append(data);
+									});
+								});
+							});
+							</script>
+						</div>
+
+                        <div class="table-responsive-md">
+                            <table class="table table-hover table-striped servicetable">
+								<thead>
+									<tr>
+										<th class="text-center" style="width:150px;"><?php echo t('country'); ?> <span class="text-danger">*</span></th>
+										<th class="text-center" style="width:150px;"><?php echo t('zone'); ?> <span class="text-danger">*</span></th>
+										<th class="text-center"><?php echo t('servicename'); ?></th>
+										<th class="text-center" style="width:130px;"><?php echo t('cost'); ?></th>
+										<th class="text-center" style="width:130px;">ETD <i class="fe fe-info shiza_tooltip text-blue" title="<?php echo t('estimatedtimeofdelivery'); ?>"></i></th>
+										<th class="text-center" style="width:200px;"><?php echo t('note'); ?></th>
+										<th style="width:20px;" class="text-center"><i class="fe fe-settings"></i></th>
+									</tr>
+								</thead>
+								<tbody>
+                                    <tr id="novalfound"><td colspan="7" class="text-center"><h5 class="py-2"><?php echo t('nodatafound'); ?></h5></td></tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class="card-footer">
+                <button type="submit" class="btn btn-primary"><i class="fe fe-plus"></i> <?php echo t('btnadd'); ?></button>    
+            </div>
+        </div>
+
+    </div>
+
+</div>
+<?php
+endif;
+echo form_close();
 
 include V_ADMIN_PATH . "footer.php";

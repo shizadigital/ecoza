55d6ddbdcee5071353043e2b5afdc8dc7d736c21
Afrioni Jaya Saputra <afrioni01@gmail.com>
2020-06-20 02:34:44 +0700

    add library in template

--- application/core/MY_Loader.php
+++ application/core/MY_Loader.php
@@ -76,4 +76,60 @@ class MY_Loader extends MX_Loader {
         foreach ($helpers as $_helper) $this->helper($_helper);
         return $this;
     }
+
+    /** Load a module library **/
+    public function library($library, $params = NULL, $object_name = NULL)
+    {
+        if (is_array($library)) return $this->libraries($library);
+
+        $class = strtolower(basename($library));
+
+        if (isset($this->_ci_classes[$class]) && $_alias = $this->_ci_classes[$class])
+            return $this;
+
+        ($_alias = strtolower($object_name)) OR $_alias = $class;
+
+        list($path, $_library) = Modules::find($library, $this->_module, 'libraries/');
+
+        if($this->CI->config->item('admin_slug') != $this->CI->uri->segment(1)){
+
+            $templatedir = FCPATH .'templates/'.$this->get_template();  
+            if(file_exists($templatedir.'/libraries/'.$library.EXT) != FALSE){
+                $path = $templatedir.'/libraries/';
+            }
+        }
+
+        /* load library config file as params */
+        if ($params == NULL)
+        {
+            list($path2, $file) = Modules::find($_alias, $this->_module, 'config/');
+            ($path2) && $params = Modules::load_file($file, $path2, 'config');
+        }
+
+        if ($path === FALSE)
+        {
+            $this->_ci_load_library($library, $params, $object_name);
+        }
+        else
+        {
+            Modules::load_file($_library, $path);
+
+            $library = ucfirst($_library);
+            CI::$APP->$_alias = new $library($params);
+
+            $this->_ci_classes[$class] = $_alias;
+        }
+        return $this;
+    }
+
+    /** Load an array of libraries **/
+    public function libraries($libraries)
+    {
+        foreach ($libraries as $library => $alias) 
+        {
+            (is_int($library)) ? $this->library($alias) : $this->library($library, NULL, $alias);
+        }
+        return $this;
+    }
+
 }

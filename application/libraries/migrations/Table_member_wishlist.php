96ad10e6cf73b5460f1e16342d3d160f59628595
Afrioni Jaya Saputra <afrioni@afrioni.web.id>
2020-09-06 20:17:06 +0700

    added wishlist table

---
+++ application/libraries/migrations/Table_member_wishlist.php
@@ -0,0 +1,41 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Table_member_wishlist {
+	/**
+	 * !!! CAUTION !!!
+	 * 
+	 * Don't change the table name and class name because to important to seeder system
+	 * 
+	 * if you want to change the table name, copy your script code in this file
+	 * remove this file with this bash 
+	 * 
+	 * php index.php Migration remove {table name}
+	 * 
+	 * then create new database with migration bash and paste you code before
+	 */
+
+	private $CI;
+
+	public function __construct(){
+		$this->CI =& get_instance();
+
+        $this->CI->load->model('mc');
+        $this->CI->load->library('Schema');
+	}
+
+	public function migrate(){
+		$schema = $this->CI->schema->create_table('member_wishlist');
+		$schema->increments('wlId',  ['type' => 'BIGINT', 'length' => '30']);
+        $schema->integer('mId', ['length' => '11', 'unsigned' => TRUE]);
+		$schema->integer('prodId', ['length' => '11', 'unsigned' => TRUE]);
+		$schema->integer('wlAdded', ['length' => '11', 'unsigned' => TRUE]);
+		$schema->run();
+	}
+
+	public function seeder(){
+		
+	}
+
+}
+
